9496438bd667890a11afc9325211ee5dac6f0f0b
Tabris <tabris.eth@gmail.com>
2022-04-24 22:11:09 +0800

    update uniswap brownie

---
+++ uniswap/v2_brownie/scripts/deploy.py
@@ -0,0 +1,25 @@
+from brownie import Contract, UniswapV2ERC20
+
+from web3 import Web3
+import time, sys
+from scripts.helpful_scripts import get_accounts
+
+
+def set_env():
+    """
+    设置一些环境变量，并且设置为global
+    """
+    global deployer, account1
+    [deployer, account1] = get_accounts(2)
+    return
+
+
+def deploy_token():
+    token_contract = UniswapV2ERC20.deploy({"from": deployer})
+    print(f"token_contract deployed: {token_contract}")
+    return
+
+
+def main():
+    set_env()
+    deploy_token()
